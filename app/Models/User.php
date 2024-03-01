9037cf0aabb450bf5904b5f930aeab95e037f0ce
sugisuartanaya <sugisuartanaya@gmail.com>
2024-03-02 01:11:25 +0800

    start project

---
+++ app/Models/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'username',
+        'password',
+    ];
+    
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+}
